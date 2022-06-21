e31d625315169459312b6222c32ec3b502e0351b
Csc32 <sanzonettycar@gmail.com>
2022-06-21 17:58:14 -0400

    Create the method loadModel()

--- libs/controller.php
+++ libs/controller.php
@@ -4,6 +4,18 @@
             $this->view = new View();
         }
 
+        function loadModel($model){
+                $url = 'models/'.$model."/model.php";
+                echo $url;
+
+                if(file_exists($url)){
+                    require $url;
+
+                    $modelName   = $model.'Model';
+
+                    $this->model = new $modelName();
+                }
+        }
       }
 
 ?>
