10fabf917fe7ee790dc13dafe96171909532660c
Csc32 <sanzonettycar@gmail.com>
2022-06-21 17:45:06 -0400

    Create config folder and adding some constants

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('URL','http://localhost/cursoMVC_PHP/');
+    define('HOST','localhost');
+    define('PASSWORD','12345');
+    define('USER','postgres');
+    define('DBNAME','people');
+?>
