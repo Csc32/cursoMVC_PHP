1c8d56c1879f0645096d6585a9a73dc280ee2f3b
Csc32 <sanzonettycar@gmail.com>
2022-06-21 17:56:27 -0400

    adding conditional and call the method loadModel()

--- libs/app.php
+++ libs/app.php
@@ -8,14 +8,17 @@
             $url = isset($_GET['url']) ? $_GET['url'] : "main";
             $url = rtrim($url,"/");
             $url = explode("/",$url);
-
+            if($url == "main"){ 
+                $controller = new $url;
+                $controller->loadModel('main');
+            }
             //var_dump($url);
-
             $fileController = "controllers/" . $url[0] . ".php";
-
+      
             if(file_exists($fileController)){
                 require_once $fileController;
                 $controller = new $url[0];
+                $controller->loadModel($url[0]);
                 if(isset($url[1])){
                     $controller->{$url[1]}();
                 }
