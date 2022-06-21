ef2bf3ab78a8ee6056d55f727fe3793e1ba50521
Csc32 <sanzonettycar@gmail.com>
2022-06-21 17:49:04 -0400

    adding url to the links

--- views/nav.php
+++ views/nav.php
@@ -2,10 +2,10 @@
 <header class="header">
     <nav>
         <ul>
-            <li class="item"><a href="index">Index</a></li>
-            <li class="item"><a href="main">Main</a></li>
-            <li class="item"><a href="contact">Contact</a></li>
-            <li class="item"><a href="help">Help</a></li>
+            <li class="item"><a href="<?= constant('URL') ?>newUser">new User</a></li>
+            <li class="item"><a href="<?= constant('URL') ?>main">Main</a></li>
+            <li class="item"><a href="<?= constant('URL') ?>contact">Contact</a></li>
+            <li class="item"><a href="<?= constant('URL') ?>help">Help</a></li>
         </ul>
     </nav>
 </header>
