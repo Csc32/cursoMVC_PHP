e853ec2a3a7c9d29697268bd3ad8e29b509381bc
Csc32 <sanzonettycar@gmail.com>
2022-06-21 17:50:00 -0400

    modify css link

--- views/header.php
+++ views/header.php
@@ -5,6 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MVC Course</title>
-    <link rel="stylesheet" href="./views/style/styles.css">
+    <link rel="stylesheet" href="<?= constant('URL');?>views/style/styles.css">
 </head>
 <body>
